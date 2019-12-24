77c0960d731a29d22cc5422b859cc41780828477
df7c5117 <df7c5117@gmail.com>
2019-12-25 01:28:01 +0800

    update

--- js/nightshift.js
+++ js/nightshift.js
@@ -4,9 +4,41 @@ var doc_head = document.head;
 var link_style = document.createElement("link");
 link_style.setAttribute("rel","stylesheet");
 link_style.setAttribute("type","text/css");
-if (dHours > 21 || dHours < 6 ){
-    link_style.setAttribute("href","./css/night.css");
+
+if ((dHours > 21 || dHours < 6)&& sessionStorage.inverse == 0){
+	var isNight = 1;
+}else if ((dHours > 21 || dHours < 6)&& sessionStorage.inverse == 1){
+	var isNight = 0;
+}else if ((dHours <= 21 && dHours >= 6)&& sessionStorage.inverse == 0){
+	var isNight = 0;
 }else{
+	var isNight = 1;
+}
+
+function changeTheme(){
+	if (isNight){
+		day();
+		isNight = 0;
+		sessionStorage.inverse = 1;
+	}else{
+		night();
+		isNight = 1;
+		sessionStorage.inverse = 0;
+	}
+}
+
+if (isNight){
+	night();
+}else{
+	day();
+}
+
+function night(){
+	link_style.setAttribute("href","./css/night.css");
+}
+function day(){
 	link_style.setAttribute("href","./css/day.css");
 }
 doc_head.appendChild(link_style);
+
+
